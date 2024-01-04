5ba1421eccb0d3248e56cde5307ed00c8e3cc7ba
b5029675 <b5029675@my.shu.ac.uk>
2024-01-04 21:51:49 +0000

    Base.html and placeholder bootstrap

--- itreporting/views.py
+++ itreporting/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'itreporting/home.html')
+    return render(request, 'itreporting/home.html', {'title':'Home'})
 
 def about(request):
-    return render(request, 'itreporting/about.html')
+    return render(request, 'itreporting/about.html', {'title':'About'})
 
 def contact(request):
-    return render(request, 'itreporting/contact.html')
+    return render(request, 'itreporting/contact.html', {'title':'Contact'})
