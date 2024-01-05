5156fa7d91f48cfc7cdf72e38b7bbb2108683941
b5029675 <b5029675@my.shu.ac.uk>
2024-01-05 01:00:14 +0000

    User account and login with fields

--- itapps/settings.py
+++ itapps/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'itreporting.apps.ItreportingConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
