fb691759633f3f1f9d90d0f5ee6402ec25f8cf11
b5029675 <b5029675@my.shu.ac.uk>
2024-01-10 11:20:47 +0000

    Login profile authentication

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -19,3 +20,7 @@ def register(request):
     else:
         form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form , 'title': 'Student Registration'})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html', {'title': 'Student Profile'})
