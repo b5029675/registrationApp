4d82f6a2f8d5f720b89028b7e5a823af604a53bf
b5029675 <b5029675@my.shu.ac.uk>
2024-01-12 12:31:51 +0000

    Student Model updated

--- users/models.py
+++ users/models.py
@@ -6,6 +6,11 @@ from django.contrib.auth.models import User
 class Student(models.Model):
     user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
     image = models.ImageField(default = 'default.png', upload_to = 'profile_pics')
+    dateBirth = models.DateField(null = True)
+    address = models.CharField(max_length = 20 ,null = True)
+    citytown = models.CharField(max_length = 20 ,null = True)
+    country = models.CharField(max_length = 20 ,null = True)
+
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
