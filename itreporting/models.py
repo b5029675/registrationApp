eca64b18b29a8b400e748ee160dbc5ca6408348f
b5029675 <b5029675@my.shu.ac.uk>
2024-01-16 15:42:02 +0000

    Models refined

--- itreporting/models.py
+++ itreporting/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class Course(models.Model):
     name = models.TextField()
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Module(models.Model):
     name = models.CharField(max_length = 15, primary_key = "name")
     code = models.CharField(max_length = 9)
@@ -16,11 +19,16 @@ class Module(models.Model):
     availability = models.BooleanField(default = True)
     coursesAllowed = models.ManyToManyField(Course)
 
+    def __str__(self):
+        return f'{self.name}'
+    
 class Registration(models.Model):
     user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
     module = models.OneToOneField(Module)
     dateRegistered = models.DateTimeField(default = timezone.now)
-
+    
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
 
 
     #room = models.CharField(max_length = 100)
